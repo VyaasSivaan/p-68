a5f5f291f30b700de5a11122d14ebe952cc9f8ee
VyaasSivaan <118091428+VyaasSivaan@users.noreply.github.com>
2024-02-04 20:27:07 +1100

    Add files via upload

---
+++ main.java
@@ -0,0 +1,12 @@
+// This is a placeholder for your Java code
+// You can add your Java code related to your web application here
+
+public class Main {
+  public static void main(String[] args) {
+    // Your main Java code goes here
+    System.out.println("Hello, World!");
+  }
+}
+
+
+
